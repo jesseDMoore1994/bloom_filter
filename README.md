83c7b159cef4cf8ec9ea7d7a5715dcafd9886e8b
Jesse Moore <jaymen1994@hotmail.com>
2020-02-23 18:09:02 -0600

    add readme and add user input guards

---
+++ README.md
@@ -0,0 +1,37 @@
+# Bloom filter password checker.
+
+## How to build the project
+
+First, make sure the current working directory is at the root of the dirctory. (This README is at
+ the project root)
+
+From the root of the project, execute the following commands.
+
+```
+take build
+cmake .. -G "Unix Makefiles"
+make
+```
+
+This generates the source binaries at the following location from the project root, ./build/src
+
+## How to run the project
+
+First, make sure the project has been built using the project above.
+
+From the root of the project, navigate to the built binaries.
+
+```
+cd ./build/src
+```
+
+Place the dictionary file that you want to use in this directory with the name "dictionary.txt"
+NOTE: DICTIONARY FILE MUST BE NAMED "dictionary.txt" AND MUST BE IN THE DIRECTORY LISTED ABOVE 
+  RELATIVE TO PROJECT ROOT
+
+Once you have placed the dictionary in the directory, execute the program by running the following
+ command
+
+```
+./bloom_filter
+```
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,37 @@
 
 int main() {
     bloomFilter bf = bloomFilter(dicReader().getDictLines());
+    bool dirty_string_detected = false;
+    auto clean_string = [&dirty_string_detected](std::string dirty_string) {
+        std::string clean_string = "";
+        for (char const& c: dirty_string) {
+            if(c >= 'A' && c <= 'Z')
+                clean_string += c;
+            else
+                dirty_string_detected = true;
+        }
+        return clean_string;
+    };
 
     std::string user_input;
     std::cout << "Enter new password: ";
     std::cin >> user_input;
+    user_input = clean_string(user_input);
+    if(dirty_string_detected) {
+        std::cout << "Your string was dirty, cleaned to \"" << user_input << "\".\n";
+        dirty_string_detected = false;
+    }
 
     bool new_passwd_is_in_table = bf.isInTable(user_input);
 
     while (new_passwd_is_in_table) {
         std::cout << "The password you entered is unacceptable, enter new password: ";
         std::cin >> user_input;
+        user_input = clean_string(user_input);
+        if(dirty_string_detected) {
+            std::cout << "Your string was dirty, cleaned to \"" << user_input << "\".\n";
+            dirty_string_detected = false;
+        }
         new_passwd_is_in_table = bf.isInTable(user_input);
     }
 
